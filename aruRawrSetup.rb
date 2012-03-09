581605d6f07302a2bc0fc1b83e1c47335d33f8e9
Allister Banks <arubdesu@me.com>
2012-03-09 11:24:18 -0500

    work in progress, 2012 stylee

--- aruRawrSetup.rb
+++ aruRawrSetup.rb
@@ -1,25 +1,55 @@
-# mapping out new machine setup
+# updated, eula-accept dmg handling thanks to j2fly, https://github.com/benhoskings/babushka/issues/136#issuecomment-2177849
+
+meta 'eula_app' do
+  accepts_value_for :app_name, :basename
+  accepts_value_for :source, :source
+  accepts_value_for :dmg_name
+
+  template {
+    met? {
+      "/Applications/#{app_name}".p.exist?
+    }
+    meet {
+      log "Using Babushka's Resource.get to snatch #{app_name}"
+      Babushka::Resource.get("#{source}") do end
+      log_shell "Stripping EULA","/usr/bin/hdiutil convert -quiet ~/.babushka/downloads/#{dmg_name} -format UDTO -o ~/.babushka/downloads/app"
+      log_shell "Mounting and creating local folder with contents of DMG","/usr/bin/hdiutil attach -quiet -nobrowse -noverify -noautoopen -mountpoint ~/.babushka/downloads/app ~/.babushka/downloads/app.cdr"
+      log_shell "Copying into /Applications","sudo cp -r ~/.babushka/downloads/app/*.app /Applications", :spinner => true
+
+      after {
+        log "Detaching DMG and deleting the .cdr we created"
+        shell("/usr/bin/hdiutil detach ~/.babushka/downloads/app/")
+        "~/.babushka/downloads/app.cdr".p.remove
+      }
+    }
+  }
+end
 
 dep 'aruRawrSetup' do
-  requires 'Google Chrome.app', 'Firefox.app', 'YoruFukurou.app', 'Carbon Copy Cloner.app', 'Colloquy.app', 'KisMAC.app', 'Adium.app', 'TextExpander.app', 'Mactracker.app', 'ZimbraDesktop.installer', 'TextMate.app', 'nmap.installer', 'Growl.installer', 'CrashPlan.installer', 'VirtualBox.installer'
-end
-dep 'Google Chrome.app' do
-  source 'https://dl-ssl.google.com/chrome/mac/stable/CHFA/googlechrome.dmg'
+  requires 'Firefox.app', 'Google Chrome.app', 'Remote Desktop Connection.installer', 'ScreenSharingMenulet.app', 'OmniGraffle Professional 5.app', 'Server Admin Tools.installer', 'Carbon Copy Cloner.app', 'Adium.app', 'TextExpander.app', 'Mactracker.app', 'TextMate.app', 'nmap.installer', 'VirtualBox.installer', 'OmniDiskSweeper.app'
 end
 dep 'Firefox.app' do
-  source 'http://3347-mozilla.voxcdn.com/pub/mozilla.org/firefox/releases/5.0/mac/en-US/Firefox%205.0.dmg'
+  source 'http://mirrors.coreix.net/mozilla/firefox/releases/10.0.2/mac/en-US/Firefox%2010.0.2.dmg'
 end
-dep 'YoruFukurou.app' do
-  source 'http://aki-null.net/yf/YoruFukurou_SL.zip'
+dep 'Google Chrome.app' do
+  source 'https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg'
+end
+dep 'Remote Desktop Connection.installer' do
+  source 'http://download.microsoft.com/download/C/F/0/CF0AE39A-3307-4D39-9D50-58E699C91B2F/RDC_2.1.1_ALL.dmg'
+end
+dep 'ScreenSharingMenulet.app' do
+  source 'http://www.klieme.com/Downloads/ScreenSharingMenulet/ScreenSharingMenulet_2.2.zip'
+end
+# OmniGroup Products require EULA acceptance
+dep 'OmniGraffle Professional 5.app', :template => 'eula_app' do
+  source 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.5/OmniGrafflePro-5.2.3-English.dmg'
+  dmg_name 'OmniGrafflePro-5.3.3.dmg'
+end
+dep 'Server Admin Tools.installer' do
+  source 'http://support.apple.com/downloads/DL1488/en_US/AdminTools10.7.3.dmg'
 end
 dep 'Carbon Copy Cloner.app' do
-  source 'http://web.mac.com/bombich/ccc-3.3.7.dmg'
-end
-dep 'Colloquy.app' do
-  source 'http://colloquy.info/downloads/colloquy-latest.zip'
-end
-dep 'KisMAC.app' do
-  source 'http://update.kismacmirror.com/binaries/KisMAC-0.3.3.dmg'
+  source 'http://web.mac.com/bombich/ccc-3.4.4.dmg'
 end
 dep 'Adium.app' do
   source 'http://download.adium.im/Adium_1.4.2.dmg'
@@ -30,50 +60,48 @@ end
 dep 'Mactracker.app' do
   source 'http://www.mactracker.ca/downloads/Mactracker_6.0.2.zip'
 end
-dep 'ZimbraDesktop.installer' do
-  source 'http://files2.zimbra.com/downloads/zdesktop/7.0.1/b10791/zdesktop_7_0_1_b10791_macos_intel.dmg'
-# Test that the actual .app bundle is in the right place.
-  met? { "/Applications/Zimbra Desktop/Zimbra Desktop.app".p.exists? }
-end
 dep 'TextMate.app' do
   source 'http://download.macromates.com/TextMate_1.5.10.zip'
 end
 dep 'nmap.installer' do
   source 'http://nmap.org/dist/nmap-5.51.dmg'
 end
-dep 'Growl.installer' do
-  source 'http://growl.cachefly.net/Growl-1.2.2.dmg'
-  provides 'growlnotify'
-  # Test that the prefPane is in the right place.
-  # TODO: get HardwareGrowler out of Extras folder in DMG, add to login items, run, and hide dock icon
-  met? { "/Library/PreferencePanes/Growl.prefPane".p.exists? }
-end
-dep 'CrashPlan.installer' do
-  source 'http://download.crashplan.com/installs/mac/install/CrashPlanPRO/CrashPlanPRO_2010-03-08_Mac.dmg'
-  met? { "/Applications/CrashPlan.app".p.exists? }
-end
 dep 'VirtualBox.installer' do
   source 'http://download.virtualbox.org/virtualbox/4.0.10/VirtualBox-4.0.10-72479-OSX.dmg'
 end
 
-# This had some sort of version number bug
-# dep 'Remote Desktop Connection.installer' do
-#   source 'http://download.microsoft.com/download/C/F/0/CF0AE39A-3307-4D39-9D50-58E699C91B2F/RDC_2.1.1_ALL.dmg'
-# end
-# 
-# These both require license agreements before drag-drop
-# dep 'OmniGraffle.app' do
-#   source 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.5/OmniGrafflePro-5.2.3-English.dmg'
-# end
-# dep 'OmniDiskSweeper.app' do
-#   source 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.4/OmniDiskSweeper-1.7.2.dmg'
-# end
+
+dep 'OmniDiskSweeper.app', :template => 'eula_app' do
+  source 'http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.4/OmniDiskSweeper-1.7.2.dmg'
+  dmg_name 'OmniGrafflePro-5.3.3.dmg'
+end
 #
 # + iWork from local network, 
 # http://support.apple.com/downloads/DL1097/en_US/iWork9.0.5Update.dmg
-# + ARD and ServerAdmin Tools from  dev site
-# + DeployStudio admin/runtime once I test new rev
-# + VPNTracker requires acct login to download, http://www.equinux.com/us/products/vpntracker/download.html
+# Xcode and PackageMaker from offline pkg
+# ARD, ServerAdmin Tools, command line tools for Xcode from  dev site
+# 'Right' version of VMWare from my Dropbox
+# Repackaged SuspisciousPackage(meta!)
 # TODO: do a before mkdir for ~/Library/Extensions to add http://www.verticalforest.com/youtube5/YouTube5.safariextz
 # http://www.charlessoft.com/Pacifist_2.6.4.dmg
 # http://download.microsoft.com/download/A/A/7/AA7AB701-B35D-4EA7-A02A-BAAD536EA8CF/Microsoft%20Mouse_d291.dmg
+
+# dep 'YoruFukurou.app' do
+#   source 'http://aki-null.net/yf/YoruFukurou_SL.zip'
+# end
+# dep 'ZimbraDesktop.installer' do
+#   source 'http://files2.zimbra.com/downloads/zdesktop/7.0.1/b10791/zdesktop_7_0_1_b10791_macos_intel.dmg'
+# # Test that the actual .app bundle is in the right place.
+#   met? { "/Applications/Zimbra Desktop/Zimbra Desktop.app".p.exists? }
+# end
+# dep 'Growl.installer' do
+#   source 'http://growl.cachefly.net/Growl-1.2.2.dmg'
+#   provides 'growlnotify'
+#   # Test that the prefPane is in the right place.
+#   # TODO: get HardwareGrowler out of Extras folder in DMG, add to login items, run, and hide dock icon
+#   met? { "/Library/PreferencePanes/Growl.prefPane".p.exists? }
+# end
+# dep 'CrashPlan.installer' do
+#   source 'http://download.crashplan.com/installs/mac/install/CrashPlanPRO/CrashPlanPRO_2010-03-08_Mac.dmg'
+#   met? { "/Applications/CrashPlan.app".p.exists? }
+# end
